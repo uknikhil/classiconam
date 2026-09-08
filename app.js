363b87c485ab178c2eb82774ee1354424a3abb01
Nikhil UK <mr.nikhiluk@gmail.com>
2026-09-08 11:06:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const journey = [
   { year:'2026', title:'A new chapter begins', text:'In 2026, Prakambanam takes our Onam spirit to new heights with excitement, energy, and togetherness.', image:'onam-2026-poster.jpg' }
 ];
 const sponsors = [
-  { tier:'Title Partner', name:'Signature Dwellings', detail:'An award-winning real estate residential developer.', logo:'Signature.jfif' },
+  { tier:'Title Partner', name:'Signature Dwellings', detail:'An award-winning real estate residential developer.', logo:'Signature.jpeg' },
   { tier:'Gold Partner', name:'Divine Providence School', detail:'Co-educational, English-medium school affiliated with CBSE.', logo:'Divine.jpg' },
   { tier:'In Association With', name:'Nativehues', detail:'Branding * Designing * Book Publishing', logo:'Nativehues.jpeg' },
   { tier:'In Association With', name:'Terrart', detail:'building-materials company specializing in natural, eco-friendly and terracotta products for homes and commercial projects.', logo:'terrart.jpg' },
