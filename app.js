18e1300fc45667383f6c7e78aba01cb50b808fbf
Nikhil UK <mr.nikhiluk@gmail.com>
2026-09-09 15:29:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,9 +21,10 @@ const journey = [
 const sponsors = [
   { tier:'Title Partner', name:'Signature Dwellings', detail:'An award-winning real estate residential developer.', logo:'Signature.jpeg' },
   { tier:'Gold Partner', name:'Divine Providence School', detail:'Co-educational, English-medium school affiliated with CBSE.', logo:'Divine.jpg' },
+  { tier:'In Association With', name:'MarkON Homes', detail:'Trusted luxury villa developer in Bangalore', logo:'MarkONHomes.jpeg' },
+     { tier:'In Association With', name:'B&W Ayurvedic', detail:'Healthy Body | Healthy Mind | Radiant Beauty', logo:'BandW.jpeg' },
   { tier:'In Association With', name:'Nativehues', detail:'Branding * Designing * Book Publishing', logo:'Nativehues.jpeg' },
-  { tier:'In Association With', name:'Terrart', detail:'building-materials company specializing in natural, eco-friendly and terracotta products for homes and commercial projects.', logo:'terrart.jpg' },
-   { tier:'In Association With', name:'B&W Ayurvedic', detail:'Healthy Body | Healthy Mind | Radiant Beauty', logo:'BandW.jpeg' },
+  { tier:'In Association With', name:'Terrart', detail:'Building-materials company specializing in natural, eco-friendly and terracotta products for homes and commercial projects.', logo:'terrart.jpg' },
   { tier:'In Association With', name:'Prime Cut', detail:'Prime Cut is a fresh meat and seafood retail brand.', logo:'primecut.jpg' },
   { tier:'In Association With', name:'Ozmo Silks', detail:'The flagship family store', logo:'OzmoSilks.jpg' }
 ];
